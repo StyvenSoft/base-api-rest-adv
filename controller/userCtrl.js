48ecf5cd2bfe5c0e79485b5076f8b35f46d05c34
Steveen Echeverri <steveen.echeverri@gmail.com>
2024-01-11 23:26:10 -0500

    errorHandler base is implemented

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({ email: email });
 
@@ -9,11 +10,8 @@ const createUser = async (req, res) => {
         const newUser = await User.create(req.body);
         res.json(newUser)
     } else {
-        res.json({
-            msg: "User already exists",
-            success: false
-        })
+        throw new Error("User already exists");
     }
-};
+});
 
 module.exports = { createUser };
